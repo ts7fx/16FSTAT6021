959a9d3c16fe8a4c8979c483a71c28337b268658
Jack Song <ts7fx@virginia.edu>
2016-09-20 15:09:37 -0400

    added team assignment 4 question file

---
+++ team assignment 4/teamassign04.R
@@ -0,0 +1,54 @@
+
+###########################
+#                         #
+#   Team Assignment 4     #
+#                         #
+###########################
+
+## Please submit one set of answers per team.                    ##
+## Your answers may be submitted as an annotated R file.         ##
+## Please submit your plots in one PDF as a separate attachment. ##
+###################################################################
+
+
+#################
+## Question 1: ##
+#################
+
+# For each part of this problem, you are to create (or find) a data set with about 30 observations 
+# that is suitable for simple linear regression and satisfies the given specifications. If it is 
+# not possible, explain why it is not possible. For each part, include a plot that shows the 
+# interesting points.
+#
+#   (a) The data set has a point that is clearly visible for all four types of residuals 
+#       discussed -- standardized, studentized, PRESS, R-student.
+#   (b) The data set has a point that stands out when viewing studentized residuals but not 
+#       when viewing standardized residuals.
+#   (c) The data set has a point that stands out when viewing PRESS residuals but not when 
+#       viewing standardized residuals.
+#   (d) The data set has a point that stands out when viewing R-student residuals but not when 
+#       viewing standardized residuals.
+#   (e) The data set has a point that stands out when viewing PRESS residuals but not when 
+#       viewing studentized residuals.
+#   (f) The data set has a point that stands out when viewing R-student residuals but not when 
+#       viewing PRESS residuals.
+
+
+
+#################
+## Question 2: ##
+#################
+
+# For each part of this problem, you are to create (or find) a data set with about 30 observations 
+# that is suitable for simple linear regression and satisfies the given specifications on the variance
+# of the residuals. For each part, include a plot of your residuals that shows the required characteristic.
+#
+#   (a) The residuals have constant variance.
+#   (b) The residuals have variance proportional to E(y).
+#   (c) The residuals have variance proportional to E(y)^2.
+#   (d) The residuals have variance proportional to 1/E(y).
+#   (e) The residuals have variance proportional to C-E(y) for some constant C.
+#   (f) The residuals have variance proportional to E(y)(C-E(y)) for some constant C.
+
+
+
